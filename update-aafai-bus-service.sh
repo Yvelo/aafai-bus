4f55fad338d118f3f707c5c34323803555eb28a8
Yves-Loic Martin <YvesLoicMartin@aaf.lu>
2025-11-16 15:35:38 +0100

    Make update-aafai-bus-service.sh runnable

--- update-aafai-bus-service.sh
+++ update-aafai-bus-service.sh
@@ -9,6 +9,7 @@ cd /var/www/aafai-bus
 # 2. Pull the latest code as the 'www-data' user
 # This assumes you have set up deploy keys for www-data (Solution 2 from our chat)
 echo "Pulling latest code from repository..."
+sudo -u www-data git stash
 sudo -u www-data git reset --hard origin/master
 sudo chmod +x /var/www/aafai-bus/update-aafai-bus-service.sh
 
